70a9009bae60ea325fdeda7ffb9370676612d15e
n8tmps <n.tlmps01@gmail.com>
2024-10-17 14:13:18 -0700

    new file:   Lab4.ipynb 	modified:   NumericalMethodsCode/lagrangeInterpFD.py

--- NumericalMethodsCode/lagrangeInterpFD.py
+++ NumericalMethodsCode/lagrangeInterpFD.py
@@ -4,13 +4,14 @@
 # xp is an array that consists of x-coordinates where we want the function values approximated
 
 import numpy as np
-from newtonDivDiff import newtonDivDiff
-def lagrangeInterpFD(xx, ff, ffd, xp):
+from NumericalMethodsCode.newtonDivDiff import newtonDivDiff
+
+def lagrangeInterpFD(xx, ff, xp):
     FDcoeff, _ = newtonDivDiff(xx, ff) # call newtonDivDiff to get coeff a0, a1, ..., an
     n = len(xx)
 
     FDapprox = FDcoeff[0] # give a0 to FDapprox
-    for k in range(1, n): # loop through k = 1, ..., n
+    for k in range(1, n): # loop through k = 1, ..., n-1
         prod = (xp - xx[0])
         for m in range(1, k): # loop over m = 1, ..., k-1
             prod *= (xp - xx[m])
