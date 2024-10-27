6c72b37eff56731b24c7d5cc11cc936fbc9bc7ac
Nathaniel Talampas <110523334+n8tlmps@users.noreply.github.com>
2024-10-26 19:57:35 -0700

    Update euler.py

--- NumericalMethodsCode/euler.py
+++ NumericalMethodsCode/euler.py
@@ -1,6 +1,6 @@
 # this program does the Euler method on ODE IVPs
 #
-# Last update: Name, Date
+
 import numpy as np
 def euler(f,a,b,alpha,N):
     # step size
@@ -22,18 +22,3 @@ def euler(f,a,b,alpha,N):
         t[i] = a + i * h
     
     return t,w
-
-from CashKarpSys import CashKarpSys
-f1 = lambda t,u: np.cos(4*u[0]**2 - t**3) -4*t +u[2]
-f2 = lambda t,u: np.exp(-t)*u[0]*u[1]
-f3 = lambda t,u: u[0]*u[2]-4*t**2
-F1 = np.array([f1, f2, f3])
-a1 = -1.4
-b1 = 0.5
-alpha1 = np.array([-6, 20.1, 2.1])
-m1 = 3
-h10 = 0.2
-tol1 = 1e-2
-t1, w1 = CashKarpSys(F1, a1, b1, alpha1, m1, h10, tol1)
-print(f'[t1,w1] = ')
-print(f'{np.hstack((t1.reshape(len(t1),1),w1))}')
